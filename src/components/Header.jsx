985403a7e6032c705839ca2e95ba85313c83f492
imran khan <imranloveh01@gmail.com>
2023-08-31 02:04:53 +0500

    hook added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="max-w-6xl mx-auto flex justify-between items-center w-full">
             <Link className="text-2xl font-bold"  to="/">#VanLife</Link>
-            <nav className="bg-transparent flex md:gap-5  md:text-xl text-sm">
+            <nav className="bg-transparent flex md:gap-5 flex-col md:flex-row  md:text-xl text-sm">
                 <NavLink  to='/host' >Host</NavLink>
                 <NavLink to="/about">About</NavLink>
                 <NavLink to="/vans">Vans</NavLink>
--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,33 +1,52 @@
-import { Link } from "react-router-dom"
-import data from '../Data.json'
-import { useEffect, useState } from "react"
+import { Link, useSearchParams } from "react-router-dom";
+import data from "../Data.json";
+import { useEffect, useState } from "react";
 
 export default function Vans() {
-    const [vans, setVans] = useState([])
-    
-    useEffect(()=>{
-        setVans(data)
-    },[])
+  const [vans, setVans] = useState([]);
+  let [searchParams, setSearchParams] = useSearchParams();
 
-    const vanElements = vans.map(van => (
-        <div key={van.id} className="van-tile">
-            <Link to={`/vans/${van.id}`}>
-                <img src={van.imageUrl} />
-                <div className="van-info">
-                    <h3>{van.name}</h3>
-                    <p>${van.price}<span>/day</span></p>
-                </div>
-                <i className={`van-type ${van.type} selected`}>{van.type}</i>
-            </Link>
-        </div>
-    ))
+  const typeFilter = searchParams.get("type");
+  console.log(typeFilter);
 
-    return (
-        <div className="van-list-container max-w-6xl mx-auto">
-            <h1>Explore our van options</h1>
-            <div className="van-list">
-                {vanElements}
-            </div>
+  useEffect(() => {
+    setVans(data);
+  }, []);
+
+  const displayVans = typeFilter
+    ? vans.filter((van) => van.type.toLowerCase() === typeFilter)
+    : vans;
+
+  const vanElements = displayVans.map((van) => (
+    <div key={van.id} className="van-tile">
+      <Link to={`/vans/${van.id}`}>
+        <img className="w-[100%] object-cover" src={van.imageUrl} />
+        <div className="van-info">
+          <h3>{van.name}</h3>
+          <p className="mb-2">
+            ${van.price}
+            <span>/day</span>
+          </p>
         </div>
-    )
+      </Link>
+      <Link to={`?type=${van.type}`}>
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>{" "}
+      </Link>
+    </div>
+  ));
+
+  return (
+    <div className="van-list-container max-w-6xl mx-auto">
+      <h1>Explore our van options</h1>
+      <div className="flex gap-5 mt-10">
+        <Link to={`?type=simple`} className="bg-orange-500 text-white p-2 rounded-md">Simple</Link>
+        <Link to={`?type=rugged`} className="bg-blue-500 text-white p-2 rounded-md">Rugged</Link>
+        <Link to={`?type=luxury`} className="bg-black text-white p-2 rounded-md">Luxury</Link>
+        <Link to={'.'} className="bg-slate-600 text-white p-2 rounded-md">Clear Filter</Link>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2  gap-5 mt-10  space-x-3">
+        {vanElements}
+      </div>
+    </div>
+  );
 }
